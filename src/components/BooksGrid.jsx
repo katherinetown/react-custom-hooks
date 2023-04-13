85482eae94c1d0576909aa50cf7a02797dec996c
Katherine <hurstkatherine@hotmail.co.uk>
2023-04-13 09:57:54 +0100

    exercise 1, refactor getGoogleBooks

--- src/components/BooksGrid.jsx
+++ src/components/BooksGrid.jsx
@@ -1,74 +1,58 @@
 import { useEffect, useState } from "react";
 import BookCard from "./BookCard";
-import { getBooks } from "../api/apiCalls";
 import Modal from "react-modal";
+import useGoogleBooks from "../hooks/useGoogleBooks";
 
 const customStyles = {
-    content: {
-        top: "50%",
-        left: "50%",
-        right: "auto",
-        bottom: "auto",
-        marginRight: "-50%",
-        transform: "translate(-50%, -50%)",
-    },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
 };
 
 export default function BooksGrid({ query }) {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [books, setBooks] = useState([]);
-    const [error, setError] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-        setIsLoading(true);
-        setError(false);
-        getBooks(query)
-            .then((books) => {
-                setBooks(books);
-            })
-            .catch((err) => {
-                setError(true);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }, [query]);
+  const { isLoading, error, books } = useGoogleBooks(query);
 
-    function closeModal() {
-        setIsOpen(false);
-    }
+  function closeModal() {
+    setIsOpen(false);
+  }
 
-    if (isLoading) return <p>Loading...</p>;
-    if (error) return <p>Something went wrong...</p>;
-    return (
-        <main className="books__grid">
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-                contentLabel="Publishers other books"
-                ariaHideApp={false}
-            >
-                <div>
-                    <h2>More books by this publisher</h2>
-                    <ul>
-                        <li>Book 1</li>
-                        <li>Book 2</li>
-                        <li>Book 3</li>
-                    </ul>
-                </div>
-            </Modal>
-            {books.map((book) => {
-                return (
-                    <BookCard
-                        key={book.id}
-                        title={book.volumeInfo.title}
-                        imgUrl={book.volumeInfo}
-                        setIsOpen={setIsOpen}
-                    />
-                );
-            })}
-        </main>
-    );
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Something went wrong...</p>;
+  return (
+    <main className="books__grid">
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Publishers other books"
+        ariaHideApp={false}
+      >
+        <div>
+          <h2>More books by this publisher</h2>
+          <ul>
+            <li>Book 1</li>
+            <li>Book 2</li>
+            <li>Book 3</li>
+          </ul>
+        </div>
+      </Modal>
+      {books.map((book) => {
+        return (
+          <BookCard
+            key={book.id}
+            title={book.volumeInfo.title}
+            imgUrl={book.volumeInfo}
+            setIsOpen={setIsOpen}
+          />
+        );
+      })}
+    </main>
+  );
 }
--- src/hooks/useGoogleBooks.js
+++ src/hooks/useGoogleBooks.js
@@ -1,3 +1,25 @@
-export default function useGoogleBooks() {
-    return;
+import { useState, useEffect } from "react";
+import { getBooks } from "../api/apiCalls";
+
+export default function useGoogleBooks(query) {
+  const [books, setBooks] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(false);
+    getBooks(query)
+      .then((books) => {
+        setBooks(books);
+      })
+      .catch((err) => {
+        setError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [query]);
+
+  return { isLoading, error, books };
 }
